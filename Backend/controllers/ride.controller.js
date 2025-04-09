e6e5258e9968c933f86ce46df7fd31545cabdf8b
harbindersingh621@gmail.com <harbindersingh621@gmail.com>
2025-04-09 13:02:29 +0530

    fix: changed import for captain id

--- Backend/controllers/ride.controller.js
+++ Backend/controllers/ride.controller.js
@@ -1,133 +1,159 @@
-const rideService = require('../services/ride.service');
-const { validationResult } = require('express-validator');
-const mapService = require('../services/maps.service');
-const { sendMessageToSocketId } = require('../socket');
-const rideModel = require('../models/ride.model');
-
+const rideService = require("../services/ride.service");
+const { validationResult } = require("express-validator");
+const mapService = require("../services/maps.service");
+const { sendMessageToSocketId } = require("../socket");
+const rideModel = require("../models/ride.model");
+const verify = require("jsonwebtoken").verify;
 
 module.exports.createRide = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    const { userId, pickup, destination, vehicleType } = req.body;
+  const { pickup, destination, vehicleType } = req.body;
+  const verifyJwt = await verify(
+    req.headers.authorization.split(" ")[1],
+    process.env.JWT_SECRET
+  );
+  if (!verifyJwt) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const ride = await rideService.createRide({
+      user: verifyJwt._id,
+      pickup,
+      destination,
+      vehicleType,
+    });
+    res.status(201).json(ride);
 
-    try {
-        const ride = await rideService.createRide({ user: req.user._id, pickup, destination, vehicleType });
-        res.status(201).json(ride);
+    const pickupCoordinates = await mapService.getAddressCoordinate(pickup);
 
-        const pickupCoordinates = await mapService.getAddressCoordinate(pickup);
+    const captainsInRadius = await mapService.getCaptainsInTheRadius(
+      pickupCoordinates.ltd,
+      pickupCoordinates.lng,
+      2
+    );
 
+    ride.otp = "";
 
-
-        const captainsInRadius = await mapService.getCaptainsInTheRadius(pickupCoordinates.ltd, pickupCoordinates.lng, 2);
-
-        ride.otp = ""
-
-        const rideWithUser = await rideModel.findOne({ _id: ride._id }).populate('user');
-
-        captainsInRadius.map(captain => {
-
-            sendMessageToSocketId(captain.socketId, {
-                event: 'new-ride',
-                data: rideWithUser
-            })
-
-        })
-
-    } catch (err) {
-
-        console.log(err);
-        return res.status(500).json({ message: err.message });
-    }
-
+    const rideWithUser = await rideModel
+      .findOne({ _id: ride._id })
+      .populate("user");
+    captainsInRadius.map((captain) => {
+      sendMessageToSocketId(captain.socketId, {
+        event: "new-ride",
+        data: rideWithUser,
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: err.message });
+  }
 };
 
 module.exports.getFare = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    const { pickup, destination } = req.query;
+  const { pickup, destination } = req.query;
 
-    try {
-        const fare = await rideService.getFare(pickup, destination);
-        return res.status(200).json(fare);
-    } catch (err) {
-        return res.status(500).json({ message: err.message });
-    }
-}
+  try {
+    const fare = await rideService.getFare(pickup, destination);
+    return res.status(200).json(fare);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
 
 module.exports.confirmRide = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    const { rideId } = req.body;
+  const { rideId } = req.body;
 
-    try {
-        const ride = await rideService.confirmRide({ rideId, captain: req.captain });
+  const verifyJwt = await verify(
+    req.headers.authorization.split(" ")[1],
+    process.env.JWT_SECRET
+  );
 
-        sendMessageToSocketId(ride.user.socketId, {
-            event: 'ride-confirmed',
-            data: ride
-        })
+  try {
+    const ride = await rideService.confirmRide({
+      rideId,
+      captain: verifyJwt._id,
+    });
 
-        return res.status(200).json(ride);
-    } catch (err) {
+    sendMessageToSocketId(ride.user.socketId, {
+      event: "ride-confirmed",
+      data: ride,
+    });
 
-        console.log(err);
-        return res.status(500).json({ message: err.message });
-    }
-}
+    return res.status(200).json(ride);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: err.message });
+  }
+};
 
 module.exports.startRide = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    const { rideId, otp } = req.query;
+  const { rideId, otp } = req.query;
 
-    try {
-        const ride = await rideService.startRide({ rideId, otp, captain: req.captain });
+  const verifyJwt = await verify(
+    req.headers.authorization.split(" ")[1],
+    process.env.JWT_SECRET
+  );
 
-        console.log(ride);
+  try {
+    const ride = await rideService.startRide({
+      rideId,
+      otp,
+      captain: verifyJwt._id,
+    });
 
-        sendMessageToSocketId(ride.user.socketId, {
-            event: 'ride-started',
-            data: ride
-        })
+    sendMessageToSocketId(ride.user.socketId, {
+      event: "ride-started",
+      data: ride,
+    });
 
-        return res.status(200).json(ride);
-    } catch (err) {
-        return res.status(500).json({ message: err.message });
-    }
-}
+    return res.status(200).json(ride);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
 
 module.exports.endRide = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-    const { rideId } = req.body;
+  const { rideId } = req.body;
+  const verifyJwt = await verify(
+    req.headers.authorization.split(" ")[1],
+    process.env.JWT_SECRET
+  );
 
-    try {
-        const ride = await rideService.endRide({ rideId, captain: req.captain });
+  try {
+    const ride = await rideService.endRide({ rideId, captain: verifyJwt });
 
-        sendMessageToSocketId(ride.user.socketId, {
-            event: 'ride-ended',
-            data: ride
-        })
+    sendMessageToSocketId(ride.user.socketId, {
+      event: "ride-ended",
+      data: ride,
+    });
 
-
-
-        return res.status(200).json(ride);
-    } catch (err) {
-        return res.status(500).json({ message: err.message });
-    } s
-}
+    return res.status(200).json(ride);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  s;
+};
--- frontend/src/pages/Riding.jsx
+++ frontend/src/pages/Riding.jsx
@@ -1,73 +1,84 @@
-import React, { useEffect, useContext } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { SocketContext } from '../context/SocketContext';
-import LiveTracking from '../components/LiveTracking';
+import React, { useEffect, useContext } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { SocketContext } from "../context/SocketContext";
+import LiveTracking from "../components/LiveTracking";
 
 const Riding = () => {
-    const location = useLocation();
-    const { ride } = location.state || {};
-    const { socket } = useContext(SocketContext);
-    const navigate = useNavigate();
+  const location = useLocation();
+  const { ride } = location.state || {};
+  const { socket } = useContext(SocketContext);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (socket) {
-            socket.on("ride-ended", () => {
-                navigate('/home');
-            });
+  useEffect(() => {
+    if (socket) {
+      socket.on("ride-ended", () => {
+        navigate("/home");
+      });
 
-            return () => socket.off("ride-ended");
-        }
-    }, [socket, navigate]);
+      return () => socket.off("ride-ended");
+    }
+  }, [socket, navigate]);
 
-    return (
-        <div className='h-screen'>
-            <Link to='/home' className='fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full'>
-                <i className="text-lg font-medium ri-home-5-line"></i>
-            </Link>
+  return (
+    <div className="h-screen">
+      <Link
+        to="/home"
+        className="fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full"
+      >
+        <i className="text-lg font-medium ri-home-5-line"></i>
+      </Link>
 
-            {/* Pass pickup & destination to LiveTracking */}
-            <div className='h-1/2'>
-                <LiveTracking
-                    pickup={ride?.pickupLocation}
-                    destination={ride?.destinationLocation}
-                />
-            </div>
+      {/* Pass pickup & destination to LiveTracking */}
+      <div className="h-1/2">
+        <LiveTracking
+          pickup={ride?.pickupLocation}
+          destination={ride?.destinationLocation}
+        />
+      </div>
 
-            <div className='h-1/2 p-4'>
-                <div className='flex items-center justify-between'>
-                    <img className='h-12' src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg" alt="vehicle" />
-                    <div className='text-right'>
-                        <h2 className='text-lg font-medium capitalize'>{ride?.captain.fullname.firstname}</h2>
-                        <h4 className='text-xl font-semibold -mt-1 -mb-1'>{ride?.captain.vehicle.plate}</h4>
-                        <p className='text-sm text-gray-600'>Maruti Suzuki Alto</p>
-                    </div>
-                </div>
-
-                <div className='flex gap-2 justify-between flex-col items-center'>
-                    <div className='w-full mt-5'>
-                        <div className='flex items-center gap-5 p-3 border-b-2'>
-                            <i className="text-lg ri-map-pin-2-fill"></i>
-                            <div>
-                                <h3 className='text-lg font-medium'>562/11-A</h3>
-                                <p className='text-sm -mt-1 text-gray-600'>{ride?.destination}</p>
-                            </div>
-                        </div>
-                        <div className='flex items-center gap-5 p-3'>
-                            <i className="ri-currency-line"></i>
-                            <div>
-                                <h3 className='text-lg font-medium'>₹{ride?.fare}</h3>
-                                <p className='text-sm -mt-1 text-gray-600'>Cash</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <button className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'>
-                    Make a Payment
-                </button>
-            </div>
+      <div className="h-1/2 p-4">
+        <div className="flex items-center justify-between">
+          <img
+            className="h-12"
+            src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg"
+            alt="vehicle"
+          />
+          <div className="text-right">
+            {/* <h2 className='text-lg font-medium capitalize'>{ride?.captain.fullname.firstname}</h2> */}
+            <h2 className="text-lg font-medium capitalize">Umesh Thakur</h2>
+            {/* <h4 className='text-xl font-semibold -mt-1 -mb-1'>{ride?.captain.vehicle.plate}</h4> */}
+            <h4 className="text-xl font-semibold -mt-1 -mb-1">HP 91 9991</h4>
+            <p className="text-sm text-gray-600">BMW M5 cs</p>
+          </div>
         </div>
-    );
+
+        <div className="flex gap-2 justify-between flex-col items-center">
+          <div className="w-full mt-5">
+            <div className="flex items-center gap-5 p-3 border-b-2">
+              <i className="text-lg ri-map-pin-2-fill"></i>
+              <div>
+                <h3 className="text-lg font-medium">562/11-A</h3>
+                <p className="text-sm -mt-1 text-gray-600">
+                  {ride?.destination}
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center gap-5 p-3">
+              <i className="ri-currency-line"></i>
+              <div>
+                <h3 className="text-lg font-medium">₹{ride?.fare}</h3>
+                <p className="text-sm -mt-1 text-gray-600">Cash</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <button className="w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg">
+          Make a Payment
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Riding;
